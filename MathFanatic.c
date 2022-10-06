428683ea09c53172cc17908c0cd17865011723b7
宇承 張 <andrew99154@gmail.com>
2022-10-06 09:33:58 +0800

    upd

--- MathFanatic.c
+++ MathFanatic.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
-    int n,tmp[100010],stmp[100010];
+    int n;
+    long tmp[100010],stmp[100010];
     scanf("%d",&n);
-    for(int i=0;i<n;i++)scanf("%d",&tmp[i]);
+    for(int i=0;i<n;i++)scanf("%ld",&tmp[i]);
     stmp[0] = tmp[0];
     for(int i=1;i<n;i++)stmp[i] = stmp[i-1]+tmp[i];
     for(int i=0;i<n;i++){
-        if(stmp[i]%tmp[i]==0)printf("%d %d\n",stmp[i],tmp[i]);
+        if(stmp[i]%tmp[i]==0)printf("%ld %ld\n",stmp[i],tmp[i]);
     }
 }
